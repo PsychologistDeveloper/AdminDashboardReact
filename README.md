f7eebab0f0905d583a08e3e110f17676945fb362
MaksimGritcenko <zmoaty10@gmail.com>
2021-09-14 00:10:42 +0300

    initial commit :)

---
+++ README.md
@@ -0,0 +1,17 @@
+# React-adminka
+
+Intended as admin panel based on React
+
+## Installation
+
+Run yarn to install module
+
+```bash
+yarn
+
+# start in the development environment
+yarn start
+
+# make a production build
+yarn build
+```
---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = () => ({
+  context: __dirname,
+  mode: "development",
+	entry: path.resolve(__dirname, 'src', 'index.js'),
+  devtool: 'inline-source-map',
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "bundle.js",
+		chunkFilename: "[name].js",
+    publicPath: '/'
+	},
+  devServer: {
+    host: "localhost",
+    port: 3000,
+    historyApiFallback: true,
+    open: true,
+    hot: true
+	},
+	module: {
+		rules: [
+			{
+        test: /\.(jsx|js)$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                ['@babel/preset-env', {
+                  "targets": "defaults"
+                }],
+                '@babel/preset-react'
+              ],
+            }
+          },
+          {
+            loader: 'eslint-loader',
+            options: {
+              fix: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.s?[c|a]ss$/i,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          'postcss-loader',
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.(png|j?g|svg|gif)?$/,
+        use: 'file-loader'
+      }
+		],
+	},
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+      filename: 'index.html'
+    })
+  ],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  }
+});
