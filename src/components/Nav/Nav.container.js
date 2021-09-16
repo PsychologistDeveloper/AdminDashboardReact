ffd48bdc312c254ea39780eca66ca2c781e84b78
MaksimGritcenko <zmoaty10@gmail.com>
2021-09-16 21:44:40 +0300

    AR-10: PR code review fixes

--- src/components/Nav/Nav.container.js
+++ src/components/Nav/Nav.container.js
@@ -1,13 +1,5 @@
-/* eslint-disable */
 import React from 'react';
 import NavComponent from 'Components/Nav/Nav.component';
 
-export const NavContainer = () =>
-// const { email } = props;
-
-// if (!email) return null;
-
-  (
-    <NavComponent />
-  );
+export const NavContainer = () => <NavComponent />;
 export default NavContainer;
--- src/components/NavigationTabs/NavigationTab.container.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const NavigationTabContainer = () => {
-
-}
-
-export default NavigationTabContainer;
