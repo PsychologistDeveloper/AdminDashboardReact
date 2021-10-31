3eb09f828055e6d05857938250c68cc8a56cbd84
akotikovs-sw <arturs.kotikovs@scandiweb.com>
2021-10-31 21:23:00 +0200

    AR-54. Mobile Header fix

--- src/components/Header/Header.component.js
+++ src/components/Header/Header.component.js
@@ -1,14 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import DrawerToggleButton from 'Components/DrawerToggleButton';
 
 import './Header.styles.scss';
+import Button from '@mui/material/Button';
+import { CHAT_BOARD_MOBILE_TABS } from 'Routes/ChatBoardPage/ChatBoardPage.config';
+import { updateActivePopupId } from 'Store/Popup/Popup.dispatcher';
 
-export const HeaderComponent = () => (
-  <header className="Header">
-    <h1>logo</h1>
-    <DrawerToggleButton />
-  </header>
-);
+export const mapDispatchToProps = (dispatch) => ({
+  openPopup: (popupId) => updateActivePopupId(dispatch, popupId),
+});
 
-export default HeaderComponent;
+export const HeaderComponent = ({ openPopup }) => {
+  function onSelectTabClick() {
+    openPopup(CHAT_BOARD_MOBILE_TABS);
+  }
+
+  return (
+    <header className="Header">
+      <Button
+        variant="outlined"
+        onClick={onSelectTabClick}
+        className="Tab-Button"
+      >
+        Select Tab
+      </Button>
+      <DrawerToggleButton />
+    </header>
+  );
+};
+
+export default connect(null, mapDispatchToProps)(HeaderComponent);
